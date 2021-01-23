02fba1313042df7bc67d317626eefda9a43ae4e0
Jacek Skowronek <jacek.skowronek@aptiv.com>
2021-01-23 09:25:31 +0100

    added notification_types

---
+++ include/notification_types.h
@@ -0,0 +1,18 @@
+#ifndef _NOTIFICATION_TYPES_H_
+#define _NOTIFICATION_TYPES_H_
+
+/* ============================= */
+/**
+ * @file notification_types.h
+ *
+ * @brief Common enum types related to Notification module.
+ *
+ * @author Jacek Skowronek
+ * @date 23/01/2021
+ */
+/* ============================= */
+
+#define NTF_MESSAGE_END_BYTE 0x04  /**< 0x04 (EOT) - End of transmisstion */
+
+
+#endif
