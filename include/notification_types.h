095e0bf51bcd1a26e0f1c09dd0880ab447f791cf
Jacek Skowronek <jacek.skowronek@aptiv.com>
2021-02-15 19:21:12 +0100

    Message delimiter change

--- include/notification_types.h
+++ include/notification_types.h
@@ -40,7 +40,7 @@
  */
 /* ============================= */
 #define NTF_HEADER_SIZE 3           /**< Size of header in bytes */
-#define NTF_MESSAGE_DELIMITER 0x0A  /**< 0x0A (LF) - Signalize end of message */
+#define NTF_MESSAGE_DELIMITER 0xFF  /**< 0x0A (LF) - Signalize end of message */
 #define NTF_MAX_MESSAGE_SIZE 255    /**< Maximum length of message */
 
 #define NTF_ID_OFFSET 0
